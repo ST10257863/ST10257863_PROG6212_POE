25f94212c746c55d6dd7879a291ee85d6403642c
Jamie Parker <129764378+ST10257863@users.noreply.github.com>
2024-09-09 15:01:57 +0200

    First Commit
    First commit of the default application.

---
+++ ST10257863_PROG6212_POE/Models/ErrorViewModel.cs
@@ -0,0 +1,12 @@
+namespace ST10257863_PROG6212_POE.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId
+		{
+			get; set;
+		}
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
