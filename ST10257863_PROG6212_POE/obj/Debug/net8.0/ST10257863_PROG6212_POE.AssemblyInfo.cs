4a0c9c4a906dd57f46b0dcba05a0875a4dfbfb47
Jamie Parker <129764378+ST10257863@users.noreply.github.com>
2024-10-14 15:42:51 +0200

    First commit of Part 2

--- ST10257863_PROG6212_POE/obj/Debug/net8.0/ST10257863_PROG6212_POE.AssemblyInfo.cs
+++ ST10257863_PROG6212_POE/obj/Debug/net8.0/ST10257863_PROG6212_POE.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ST10257863_PROG6212_POE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+728f99f55991d95646c3e17f73e9148e7b2cc215")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+379d5395e625bdb6d4766d91e7732a37d8b5b331")]
 [assembly: System.Reflection.AssemblyProductAttribute("ST10257863_PROG6212_POE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ST10257863_PROG6212_POE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
