cf570b6047e9cb35c69728197ee3b37e04abe5d1
Jamie Parker <129764378+ST10257863@users.noreply.github.com>
2024-11-22 00:58:45 +0200

    Fixed Manager Comment not apearing

---
+++ ST10257863_PROG6212_POE/Migrations/20241121224115_RemvoingOldStringForDocuments.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ST10257863_PROG6212_POE.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemvoingOldStringForDocuments : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SupportingDocuments",
+                table: "Claims");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SupportingDocuments",
+                table: "Claims",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
